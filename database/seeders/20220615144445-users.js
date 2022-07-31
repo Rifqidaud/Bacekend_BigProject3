877c3cdbff251e40d91481730181e482ee26ac4c
ukayaj620 <51906567+ukayaj620@users.noreply.github.com>
2022-07-31 13:36:18 +0700

    chore: new seeder

--- database/seeders/20220615144445-users.js
+++ database/seeders/20220615144445-users.js
@@ -1,21 +1,20 @@
 const uuid = require("uuid");
-const bcrypt = require('bcrypt')
+const bcrypt = require("bcrypt");
 
 module.exports = {
-  async up (queryInterface, Sequelize) {
-    const password = '12345678'
-    return queryInterface.bulkInsert("Users",[{
-      id: uuid.v4(),
-      username: "admin",
-      name:"admin",
-      password: bcrypt.hashSync(password, 12),
-      type: "admin",
-    },
+  async up(queryInterface, Sequelize) {
+    const password = "12345678";
+    return queryInterface.bulkInsert("Users", [
+      {
+        id: uuid.v4(),
+        username: "admin",
+        password: bcrypt.hashSync(password, 12),
+        type: "admin",
+      },
     ]);
   },
 
-  async down (queryInterface, Sequelize) {
-   return CountQueuingStrategyInterface.bulkDelete("Users", null);
-
+  async down(queryInterface, Sequelize) {
+    return CountQueuingStrategyInterface.bulkDelete("Users", null);
   },
 };
--- database/seeders/20220728040021-dummy-products.js
+++ database/seeders/20220728040021-dummy-products.js
@@ -8,18 +8,38 @@ module.exports = {
         {
           name: "Baju kemeja Kantoran",
           price: 12000,
-          image: "/image.png",
-          category: "Makanan Ringan",
-          description: "Chitato enak",
+          image:
+            "https://ozzakonveksi.com/wp-content/uploads/2020/12/Kemeja-Formal.jpg",
+          category: "Kemeja",
+          description: "Baju kemeja kantoran HIGH GRADE 100% LEGIT!!!",
           quantity: 10,
         },
         {
-          name: "Chitato Lite",
+          name: "Baju kemeja Kantoran B",
           price: 12000,
-          image: "/image.png",
-          category: "Makanan Ringan",
-          description: "Chitato enak",
-          quantity: 1000,
+          image:
+            "https://ozzakonveksi.com/wp-content/uploads/2020/12/Kemeja-Formal.jpg",
+          category: "Kemeja",
+          description: "Baju kemeja kantoran HIGH GRADE 100% LEGIT!!!",
+          quantity: 10,
+        },
+        {
+          name: "Baju kemeja Kantoran C",
+          price: 12000,
+          image:
+            "https://ozzakonveksi.com/wp-content/uploads/2020/12/Kemeja-Formal.jpg",
+          category: "Kemeja",
+          description: "Baju kemeja kantoran HIGH GRADE 100% LEGIT!!!",
+          quantity: 10,
+        },
+        {
+          name: "Baju kemeja Kantoran D",
+          price: 12000,
+          image:
+            "https://ozzakonveksi.com/wp-content/uploads/2020/12/Kemeja-Formal.jpg",
+          category: "Kemeja",
+          description: "Baju kemeja kantoran HIGH GRADE 100% LEGIT!!!",
+          quantity: 10,
         },
       ],
       {}
